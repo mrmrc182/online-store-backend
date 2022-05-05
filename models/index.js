81401a78d17d69cdd17a13e6f2eaad82314c1dd6
Matthew Carlson <carlson.matt.r@gmail.com>
2022-05-05 15:16:35 -0700

    starting routes

--- models/index.js
+++ models/index.js
@@ -6,34 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "product_id",
-  onDelete: "CASCADE"
+  foreignKey: "category_id"
 })
 
 // Categories have many Products
-
 Category.hasMany(Product, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE"
+  foreignKey: "category_id"
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: ProductTag,
-    unique: false
+    model: ProductTag
   },
-  as: "product_tags"
 })
 
 // Tags belongToMany Products (through ProductTag)
-
 Tag.belongsToMany(Product, {
   through: {
-    model: ProductTag,
-    unique: false
+    model: ProductTag
   },
-  as: "tags_of_products"
 })
 
 module.exports = {
